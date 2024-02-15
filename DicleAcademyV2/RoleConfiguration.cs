841818fc62cafd23d33b31b024e0936815ad5429
Serhandgr1 <dogruserhanayberk@gmail.com>
2024-02-15 20:25:36 +0300

    ConfigurationMvc

---
+++ DicleAcademyV2/RoleConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DicleAcademyV2
+{
+    public class RoleConfiguration
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                },
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                }
+            );
+        }
+    }
+}
