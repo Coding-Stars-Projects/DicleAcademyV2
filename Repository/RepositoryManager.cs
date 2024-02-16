ac61835f4b610496d7337bf7a202918e7ab8bcb3
Serhandgr1 <dogruserhanayberk@gmail.com>
2024-02-16 13:58:06 +0300

    ServiceManager

--- Repository/RepositoryManager.cs
+++ Repository/RepositoryManager.cs
@@ -13,6 +13,7 @@ namespace Repositories
     {
         private readonly RepositoryContext _context;
         private readonly Lazy<IRepositoryAboutUs> _repositoryAboutUs;
+        private readonly Lazy<IRepositoryUser> _repositoryUser;
         private readonly Lazy<IRepositoryBestCourses> _repositoryBestCourses;
         private readonly Lazy<IRepositoryContact> _repositoryContact;
         private readonly Lazy<IRepositoryCourseDetails> _repositoryCourseDetails;
@@ -31,6 +32,7 @@ namespace Repositories
         {
             _context = context;
             _repositoryAboutUs = new Lazy<IRepositoryAboutUs>(() => new RepositoryAboutUs(_context));
+            _repositoryUser = new Lazy<IRepositoryUser>(() => new RepositoryUser(_context));
             _repositoryBestCourses = new Lazy<IRepositoryBestCourses>(() => new RepositoryBestCourses(_context));
             _repositoryContact = new Lazy<IRepositoryContact>(() => new RepositoryContact(_context));
             _repositoryCourseDetails = new Lazy<IRepositoryCourseDetails>(() => new RepositoryCourseDetails(_context));
@@ -60,6 +62,10 @@ namespace Repositories
         public IRepositorySkills Skills => _repositorySkills.Value;
         public IRepositoryStudentsSay repositoryStudentsSay => _repositoryStudentsSay.Value;
         public IRepositoryWelcomeInformations WelcomeInformations => _repositoryWelcomeInformations.Value;
+
+        public IRepositoryStudentsSay StudentsSay => _repositoryStudentsSay.Value;
+
+        public IRepositoryUser User => _repositoryUser.Value;
 
         public void Save()
         {
