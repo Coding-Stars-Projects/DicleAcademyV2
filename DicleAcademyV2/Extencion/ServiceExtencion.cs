553cbf3fb9e4cb33bf47e98270ffc86bd6c30066
Serhandgr1 <dogruserhanayberk@gmail.com>
2024-02-16 14:35:03 +0300

    Referance

--- DicleAcademyV2/Extencion/ServiceExtencion.cs
+++ DicleAcademyV2/Extencion/ServiceExtencion.cs
@@ -1,8 +1,15 @@
-﻿using Entities.ModelsDto;
+﻿using Entities.Models;
+using Entities.ModelsDto;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Contracts;
 using Repositories.EF_Core;
+using Repositories.EFCore;
+using Services.Abstract;
+using Services.Contracts;
+using Services.EFCore;
 using System.ComponentModel.Design;
 using System.Reflection;
 
@@ -16,10 +23,58 @@ namespace DicleAcademyV2.Extencion
         }
         public static void ConfiguerRepostoryManager(this IServiceCollection services)
         {
-           //repostory Referanslar
+            services.AddScoped<IRepositoryAboutUs, RepositoryAboutUs>();
+            services.AddScoped<IRepositoryBestCourses, RepositoryBestCourses>();
+            services.AddScoped<IRepositoryContact, RepositoryContact>();
+            services.AddScoped<IRepositoryCourseDetails, RepositoryCourseDetails>();
+            services.AddScoped<IRepositoryCourses, RepositoryCourses>();
+            services.AddScoped<IRepositoryCoursesCategories, RepositoryCoursesCategories>();
+            services.AddScoped<IRepositoryFAQ, RepositoryFAQ>();
+            services.AddScoped<IRepositoryGallery, RepositoryGallery>();
+            services.AddScoped<IRepositoryGetInTouch, RepositoryGetInTouch>();
+            services.AddScoped<IRepositoryHeader, RepositoryHeader>();
+            services.AddScoped<IRepositoryInstructors, RepositoryInstructors>();
+            services.AddScoped<IRepositorySkills, RepositorySkills>();
+            services.AddScoped<IRepositoryStudentsSay, RepositoryStudentsSay>();
+            services.AddScoped<IRepositoryUser, RepositoryUser>();
+            services.AddScoped<IRepositoryWelcomeInformations, RepositoryWelcomeInformations>();
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+            //Repo base entitlere göre düzenlenecek hepsi eklenecek 
+            services.AddScoped<IRepositoryBase<User>, RepositoryBase<User>>();
+            services.AddScoped<IRepositoryBase<BestCourses>, RepositoryBase<BestCourses>>();
+            services.AddScoped<IRepositoryBase<Contact>, RepositoryBase<Contact>>();
+            services.AddScoped<IRepositoryBase<CourseDetails>, RepositoryBase<CourseDetails>>();
+            services.AddScoped<IRepositoryBase<Courses>, RepositoryBase<Courses>>();
+            services.AddScoped<IRepositoryBase<CoursesCategories>, RepositoryBase<CoursesCategories>>();
+            services.AddScoped<IRepositoryBase<FAQ>, RepositoryBase<FAQ>>();
+            services.AddScoped<IRepositoryBase<Gallery>, RepositoryBase<Gallery>>();
+            services.AddScoped<IRepositoryBase<GetInTouch>, RepositoryBase<GetInTouch>>();
+            services.AddScoped<IRepositoryBase<Header>, RepositoryBase<Header>>();
+            services.AddScoped<IRepositoryBase<Instructors>, RepositoryBase<Instructors>>();
+            services.AddScoped<IRepositoryBase<Skills>, RepositoryBase<Skills>>();
+            services.AddScoped<IRepositoryBase<StudentsSay>, RepositoryBase<StudentsSay>>();
+            services.AddScoped<IRepositoryBase<WelcomeInformations>, RepositoryBase<WelcomeInformations>>();
+            //repostory Referanslar
         }
         public static void ConfiguerServiceManager(this IServiceCollection services)
         { // service referanslar
+            services.AddScoped<IAboutUsService, AboutUsService>();
+            services.AddScoped<Services.Contracts.IAuthenticationService, Services.EFCore.AuthenticationService>();
+            services.AddScoped<IBestCoursesService, BestCoursesService>();
+            services.AddScoped<IContactService, ContactService>();
+            services.AddScoped<ICourseDetailsService, CourseDetailsService>();
+            services.AddScoped<ICoursesCategoriesService, CoursesCategoriesService>();
+            services.AddScoped<ICoursesService,CoursesService>();
+            services.AddScoped<IFAQService, FAQService>();
+            services.AddScoped<IGalleryService, GalleryService>();
+            services.AddScoped<IGetInTouchService, GetInTouchService>();
+            services.AddScoped<IHeaderService, HeaderService>();
+            services.AddScoped<IInstructorsService, InstructorsService>();
+            services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddScoped<ISkillsService, SkillsService>();
+            services.AddScoped<IStudentsSayService, StudentsSayService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IWelcomeInformationsService, WelcomeInformationsService>();
 
         }
         public static void ConfigureIdentity(this IServiceCollection services)
